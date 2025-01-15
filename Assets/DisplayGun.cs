ddf1cd0534746323e8d3bf9427ecf6c7aa89a61c
Valentin <114905115+Valm-epitech@users.noreply.github.com>
2025-01-15 11:57:37 +0100

    Add display or not the weapons

---
+++ Assets/DisplayGun.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HideObjectFromCamera : MonoBehaviour
+{
+    public Camera mainCamera;
+    public GameObject objectToHide;
+
+    private int originalCullingMask;
+
+    private bool Isdisplay;
+
+
+    void Start()
+    {
+        if (mainCamera == null || objectToHide == null)
+        {
+            Debug.LogError("La caméra ou l'objet à masquer n'est pas assigné.");
+            return;
+        }
+
+        objectToHide.layer = LayerMask.NameToLayer("HiddenObject");
+
+        originalCullingMask = mainCamera.cullingMask;
+
+        mainCamera.cullingMask &= ~(1 << LayerMask.NameToLayer("HiddenObject"));
+        Isdisplay = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (Isdisplay)
+            {
+                HideObjectFromCameraMethod(); 
+                Isdisplay = false;
+
+                ZombieIA[] zombies = FindObjectsOfType<ZombieIA>();
+
+                foreach (ZombieIA zombie in zombies)
+                {
+                    zombie.CanShoot = false;
+                }
+            }
+            else
+            {
+                ShowObjectToCamera();
+                Isdisplay = true;
+
+                ZombieIA[] zombies = FindObjectsOfType<ZombieIA>();
+
+                foreach (ZombieIA zombie in zombies)
+                {
+                    zombie.CanShoot = true;
+                }
+            }
+        }
+    }
+
+    public void HideObjectFromCameraMethod()
+    {
+        mainCamera.cullingMask &= ~(1 << LayerMask.NameToLayer("HiddenObject"));
+    }
+
+    public void ShowObjectToCamera()
+    {
+        mainCamera.cullingMask = originalCullingMask;
+    }
+}
--- Assets/scriptAndCanva/Zombie/ZombieIA.cs
+++ Assets/scriptAndCanva/Zombie/ZombieIA.cs
@@ -12,20 +12,19 @@ public class ZombieIA : MonoBehaviour
 
     public GameObject _TargetLook;
 
-    //Maximum de points de vie
     public int maxHitPoint = 5;
 
-    //Points de vie actuels
     public int hitPoint = 0;
 
+    public bool CanShoot;
 
-
-    // Start is called before the first frame update
     void Start()
     {
         hitPoint = maxHitPoint;
         _agent = GetComponent<NavMeshAgent>();
         _animator = GetComponent<Animator>();
+
+        CanShoot = false;
 
         _Target = GameObject.FindGameObjectWithTag("Player");
 
@@ -45,15 +44,12 @@ public class ZombieIA : MonoBehaviour
     {
         if (_Target != null && _animator.GetBool("Dead") == false)
         {
-            // Définir la destination du zombie
             _agent.SetDestination(_Target.transform.position);
 
-            // Si le zombie est assez proche du joueur, il arrête de marcher
 
             if (_agent.velocity.x == 0 && _agent.velocity.y == 0 && _agent.velocity.z == 0)
             {
                 _animator.SetBool("walk", false);
-                // On peut ici ajouter des animations pour une attaque par exemple.
             }
             else
             {
@@ -69,24 +65,24 @@ public class ZombieIA : MonoBehaviour
         }
     }
 
-    //Permet de recevoir des dommages
     public void GetDamage(int damage)
     {
-        //Applique les dommages aux points de vies actuels
-        hitPoint -= damage;
-
-        //Si les point de vie sont inférieurs à 1 = Supprime l'objet
-        if (hitPoint < 1)
+        if (CanShoot)
         {
-            _animator = GetComponentInChildren<Animator>();
-            if (_animator != null)
+
+            hitPoint -= damage;
+
+            if (hitPoint < 1)
             {
-                _animator.SetBool("Dead", true);
+                _animator = GetComponentInChildren<Animator>();
+                if (_animator != null)
+                {
+                    _animator.SetBool("Dead", true);
 
-                _agent.SetDestination(_agent.transform.position);
+                    _agent.SetDestination(_agent.transform.position);
+                }
+
             }
-
-            //Destroy(gameObject);
         }
     }
 }
