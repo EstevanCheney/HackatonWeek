c2a096d3ee3781c950b872b1383892576f3a1cda
EstevanCheney <estevan.cheney@epitech.eu>
2025-01-16 11:55:33 +0100

    [+] Add the end

--- Assets/scriptAndCanva/ChangeScene/ChangeScene.cs
+++ Assets/scriptAndCanva/ChangeScene/ChangeScene.cs
@@ -9,13 +9,16 @@ public class ChangeScene : MonoBehaviour
     [SerializeField]
     private GameObject interactionMessage;
 
+    [SerializeField]
+    private VerifAllPhotos end;
+
     private bool isPlayerIsIn;
 
     private void Update()
     {
         OnTriggerEnter(HitBox);
         OnTriggerExit(HitBox);
-        if (isPlayerIsIn && Input.GetKeyDown(KeyCode.F))
+        if (isPlayerIsIn && Input.GetKeyDown(KeyCode.F) && !end.StartEnd)
         {
             SceneManager.LoadScene("Bunker");
         }
@@ -23,7 +26,7 @@ public class ChangeScene : MonoBehaviour
     }
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !end.StartEnd)
         {
             isPlayerIsIn = true;
             interactionMessage.SetActive(true);
@@ -32,7 +35,7 @@ public class ChangeScene : MonoBehaviour
 
     void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !end.StartEnd)
         {
             isPlayerIsIn = false;
             interactionMessage.SetActive(false);
--- Assets/scriptAndCanva/EndGame/VerifAllPhotos.cs
+++ Assets/scriptAndCanva/EndGame/VerifAllPhotos.cs
@@ -5,13 +5,30 @@ public class VerifAllPhotos : MonoBehaviour
 {
     [SerializeField]
     private Inventory inventory;
+
+    [SerializeField]
+    private GameObject Lu;
+
+    [SerializeField]
+    private GameObject BoxToBunker;
+
+    [SerializeField]
+    private GameObject textToEnter;
+
+    [SerializeField]
+    private GameObject NarrationSystem;
+
+    [SerializeField]
+    private GameObject StandaloneNarration;
+
     private int count = 0;
     public bool StartEnd = false;
 
     void Update()
     {
         count = 0;
-        for (int i = 0; i < inventory.content.Count; i++) {
+        for (int i = 0; i < inventory.content.Count && count != 5; i++)
+        {
             if (inventory.content[i].name == "Photo Cathédrale")
             {
                 count++;
@@ -33,9 +50,14 @@ public class VerifAllPhotos : MonoBehaviour
                 count++;
             }
         }
-        if (count == 5) {
+        if (count == 5)
+        {
             StartEnd = true;
+            Lu.SetActive(true);
+            NarrationSystem.SetActive(true);
+            StandaloneNarration.SetActive(true);
+            textToEnter.SetActive(false);
+            BoxToBunker.SetActive(false);
         }
-        Debug.Log(count);
     }
 }
